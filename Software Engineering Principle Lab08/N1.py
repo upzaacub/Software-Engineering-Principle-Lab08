f50f0f21a262915ab25856414631c78ce2598e91
upzaacub <upzaacub@gmail.com>
2015-03-13 13:20:30 +0700

    edit

--- Software Engineering Principle Lab08/N1.py
+++ Software Engineering Principle Lab08/N1.py
@@ -1,25 +1,26 @@
 
+import sys
 from PySide import QtCore, QtGui
 
 class Simple_drawing_window(QtGui.QWidget):
     def __init__(self):
-        QWidget.__init__(self, None)
+        QtGui.QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
         self.rabbit = QtGui.QImage("images/rabbit.png")
 
     def paintEvent(self, e):
         p = QtGui.QPainter()
-        p.begin()
+        p.begin(self)
 
-        p.setPen(QColor(0, 0, 0))
-        p.setBrush(QColor(0, 127, 0))
+        p.setPen(QtGui.QColor(0, 0, 0))
+        p.setBrush(QtGui.QColor(0, 127, 0))
         p.drawPolygon([
             QtCore.QPoint(70, 100), QtCore.QPoint(100, 110),
             QtCore.QPoint(130, 100), QtCore.QPoint(100, 150),
        ])
 
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
+        p.setPen(QtGui.QColor(255, 127, 0))
+        p.setBrush(QtGui.QColor(255, 127, 0))
         p.drawPie(50, 150, 100, 100, 0, 180 * 16)
 
         p.drawPolygon([
@@ -27,3 +28,11 @@ class Simple_drawing_window(QtGui.QWidget):
         ])
 
         p.drawImage(QtCore.QRect(200, 100, 320, 320), self.rabbit)
+
+    
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = Simple_drawing_window()
+    window.show()
+    sys.exit(app.exec_())
